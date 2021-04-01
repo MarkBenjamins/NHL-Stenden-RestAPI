78721c83294cf93ebab5018304b29b74af39675e
MarkBenjamins <55874150+MarkBenjamins@users.noreply.github.com>
2021-04-01 19:43:56 +0200

    Bug fix

--- flaskProject/validator.py
+++ flaskProject/validator.py
@@ -6,11 +6,13 @@ import os
 
 dirname = os.path.dirname(__file__)
 
+
 def is_valid_xml(xml):
     filenameXML = os.path.join(dirname, "../Schemas/XSD/datasetXSD.xsd")
     xsd = XMLSchema(etree.parse(filenameXML))
     xml_object = etree.fromstring(xml)
     return xsd.validate(xml_object)
+
 
 def is_valid_json(json_string):
     filenameJSON = os.path.join(dirname, "../Schemas/JSON/datasetJSON.json")
@@ -18,12 +20,12 @@ def is_valid_json(json_string):
         try:
             schema = json.load(file)
         except json.JSONDecodeError:
-            False
+            return False
     json_object = json.loads(json_string)
     ref_resolver = jsonschema.RefResolver(base_uri='file:' + filenameJSON + '/', referrer=schema)
     try:
         jsonschema.validate(json_object, schema, resolver=ref_resolver)
-        True
+        return True
     except jsonschema.exceptions.ValidationError as errorMessage:
         print(errorMessage.message)
-        False
+        return False
